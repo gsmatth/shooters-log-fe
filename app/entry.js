8e67f3655f126b6cd10a128f12b72b6c5a748cfb
Doug Popadince <gsmatth@icloud.com>
2016-08-27 22:58:31 -0700

    added staging

--- app/entry.js
+++ app/entry.js
@@ -9,7 +9,7 @@ const angular = require('angular');
 const ngRoute = require('angular-route');
 
 // angular modules
-angular.module('demoApp', [ngRoute])
+angular.module('shuttleBuzz', [ngRoute])
 .config(['$routeProvider', function($routeProvider){
   $routeProvider
   .when('/signup', {
