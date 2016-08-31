3eca0890e213bcfbe77d42c7b90a319a97604888
muddybeard210 <mattsmith203@hotmail.com>
2016-08-30 21:31:55 -0700

    added basic basic scorecard-service and updated the index.js of scorecard-form

---
+++ app/service/scorecard-service.js
@@ -0,0 +1,36 @@
+'use strict';
+
+
+const angular = require('angular');
+
+angular.module('demoApp').factory('scorecardService', ['$log', '$q','$window', '$http', scorecardService]);
+
+
+function scorecardService($log, $q, $window, $http) {
+  let service = {};
+  let token = $window.localStorage.getItem('token');
+  service.data = [];
+
+
+  service.createCompetition = function(data) {
+    let url ='https://shooters-log-staging.herokuapp.com/api/competition';
+
+    let config = {
+      headers: {
+        'Content-Type':'application/json',
+        'Authorization':`Bearer ${token}`
+      }
+    };
+    return $http.post(url, data, config)
+    .then(res => {
+      $log.info('Success', res.data);
+      console.log(res.data);
+      return (res.data);
+    })
+    .catch(err => {
+      $log.error('Failed to return createCompetition data', err);
+      return $q.reject(err);
+    });
+  };
+  return service;
+}
