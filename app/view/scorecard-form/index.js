6efc2319795643a255fe4f2fd782834eeba07d06
Doug Popadince <gsmatth@icloud.com>
2016-08-31 19:00:26 -0700

    debugged scorecard service and added objects to scorecard controller
    modified:   app/component/menu/index.js
    modified:   app/component/menu/menu.scss
    modified:   app/entry.js
    modified:   app/service/scorecard-service.js
    modified:   app/view/scorecard-form/index.js
    modified:   app/view/scorecard-form/scorecard-form.html

--- app/view/scorecard-form/index.js
+++ app/view/scorecard-form/index.js
@@ -4,10 +4,38 @@ require('./scorecard-form.scss');
 const angular = require('angular');
 const appShooter = angular.module('appShooter');
 
-appShooter.controller('CreateScorecardFormController', ['$log', 'scorecardService', CreateScorecardFormController]);
+appShooter.controller('CreateScorecardFormController', ['$log','$scope', 'scorecardService', CreateScorecardFormController]);
 
 function CreateScorecardFormController($log, scorecardService){
+  $log.debug('enetered CreateScorecardFormController');
+  const vm = this;
 
+  vm.user = {
+    firstName:  null,
+    lastName:   null
+  };
+  $log.debug('user: ', vm.user);
+  vm.competition = {
+    location:   null,
+    action:     null,
+    caliber:    null,
+    dateOf:     null
+  };
+  $log.debug('competition: ', vm.competition);
+
+  vm.match = {
+    matchNumber:      null,
+    targetNumber:     null,
+    distanceToTarget: null,
+    relay:            null,
+    startTime:        null,
+    temperature:      null,
+    windDirection:    null,
+    windSpeed:        null,
+    lightDirection:   null,
+    weather:          null
+  };
+  $log.debug('match: ', vm.match);
 
   // this.mockCompData = {
   //   location: 'another test location',
