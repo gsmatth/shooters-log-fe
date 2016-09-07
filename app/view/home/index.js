aac9da53037e0b00b578b67eea651497700a46cc
Jacob Knaack <jacobknaack@Jacobs-MacBook-Pro-2.local>
2016-09-06 21:26:16 -0700

    can toggle between all competitions and top 5

--- app/view/home/index.js
+++ app/view/home/index.js
@@ -8,6 +8,19 @@ appShooter.controller('HomeController', ['$log', 'scorecardService', HomeControl
 
 function HomeController($log, scorecardService){
   console.log('homeCtrl hit');
+
+  this.displayAllCards = false;
+
+  this.toggle = function(){
+    this.displayAllCards = !this.displayAllCards;
+    if (this.displayAllCards === true) {
+      return this.displayScorecards = this.competitions;
+    }
+    if (this.displayAllCards === false){
+      return this.displayScorecards = this.competitions.slice(0, 1);
+    }
+  };
+
   scorecardService.getAllCompetitions()
   .then((competitions) => {
     this.competitions = competitions;
@@ -16,12 +29,14 @@ function HomeController($log, scorecardService){
       b = new Date(b.dateOf);
       return a>b ? -1 : a<b ? 1 : 0;
     });
-    this.displayScorecards = this.competitions.slice(0, 5);
+
+    this.displayScorecards = this.competitions.slice(0, 1);
+
     console.log('homeCtrl competitions', competitions);
     console.log('homeCtrl displayScorecards', this.displayScorecards);
+
   })
   .catch((err) => {
     $log.error('that failed', err);
   });
-
 }
